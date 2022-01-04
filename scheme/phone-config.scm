ce6049f37af0dece816223291ed28ea32e358500
KH405-TV <jasonmercier666@gmail.com>
2022-01-03 22:00:36 -0500

    Fixed phone.

---
+++ scheme/phone-config.scm
@@ -0,0 +1,58 @@
+#+TITLE: Guix System Configuration
+#+PROPERTY: header-args:scheme :tangle laptop-config.scm
+
+(use-modules (gnu) (guix) (guix packages) (srfi srfi-1))
+(use-package-modules bootloaders certs fonts package-management)
+(use-service-modules desktop mcron networking ssh virtualization xorg)
+
+(operating-system
+  (locale "en_US.utf8")
+  (timezone "America/New_York")
+  (keyboard-layout (keyboard-layout "us"))
+  (host-name "phone")
+  (users (cons* (user-account
+                  (name "mobile")
+                  (comment "Mobile")
+                  (group "users")
+                  (home-directory "/home/mobile")
+                  (supplementary-groups
+                    '("audio" "kvm" "netdev" "video" "wheel")))
+                %base-user-accounts))
+  (bootloader
+    (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (target "/boot/efi")
+      (keyboard-layout keyboard-layout)))
+  (mapped-devices
+    (list (mapped-device
+            (source
+              (uuid ""))
+            (target "cryptroot")
+            (type luks-device-mapping))))
+  (file-systems
+    (cons* (file-system
+             (mount-point "/")
+             (device "/dev/mapper/cryptroot")
+             (type "bcachefs")
+             (dependencies mapped-devices))
+           (file-system
+             (mount-point "/boot/efi")
+             (device (uuid "" 'fat32))
+             (type "vfat"))
+           %base-file-systems))
+  (packages
+    (append
+      (list
+        dbus
+        emacs-exwm
+        xinit
+        xrandr)
+ %my-packages))
+  (services
+    (append
+      (list (service elogind-service-type)
+            (service libvirt-service-type)
+            (service network-manager-service-type)
+            (service openssh-service-type)
+            (service wpa-supplicant-service-type)
+      %base-services))))
