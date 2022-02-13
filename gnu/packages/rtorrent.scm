31af4ad9a03e6e2b5c54cbad9e25183f2d165643
KH405-TV <jasonmercier666@gmail.com>
2022-02-13 16:12:09 -0500

    Standardization.

--- gnu/packages/rtorrent.scm
+++ gnu/packages/rtorrent.scm
@@ -1,3 +1,21 @@
+;;; GNU Guix --- Functional package management for GNU
+;;;
+;;; This file is NOT part of GNU Guix.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 (use-modules (guix packages) (gcrypt hash))
 (use-modules (gnu packages bittorrent))
 
@@ -17,3 +35,5 @@
                (base32
                 "1bs2fnf4q7mlhkhzp3i1v052v9xn8qa7g845pk9ia8hlpw207pwy")))
     (inputs (list my-libtorrent))))
+
+;;; rtorrent.scm ends here
