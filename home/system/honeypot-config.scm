8c6b46df3f0696ea8ae92c9bf471a88436e2c959
KH405-TV <jasonmercier666@gmail.com>
2022-04-15 17:01:35 -0400

    Added Honeypot

---
+++ home/system/honeypot-config.scm
@@ -0,0 +1,53 @@
+(use-modules (gnu) (guix) (guix packages) (srfi srfi-1))
+(use-service-modules mcron networking shepherd ssh)
+(use-package-modules bootloaders certs package-management)
+
+(operating-system
+  (locale "en_US.utf8")
+  (timezone "America/New_York")
+  (keyboard-layout (keyboard-layout "us" "ru"))
+  (host-name "pot")
+  (users (cons* (user-account
+                  (name "honey")
+                  (comment "honey")
+                  (group "users")
+                  (home-directory "/home/honey")
+                  (supplementary-groups
+                    '("netdev" "wheel")))
+                %base-user-accounts))
+  (bootloader
+    (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (target "/boot/efi")
+      (keyboard-layout keyboard-layout)))
+  (mapped-devices
+    (list (mapped-device
+            (source
+              (uuid ""))
+            (target "cryptroot")
+            (type luks-device-mapping))))
+  (file-systems
+    (cons* (file-system
+             (mount-point "/")
+             (device "/dev/mapper/cryptroot")
+             (type "bcachefs")
+             (dependencies mapped-devices))
+           (file-system
+             (mount-point "/boot/efi")
+             (device (uuid "" 'fat32))
+             (type "vfat"))
+           %base-file-systems))
+  (packages
+    (append
+      (list
+      )
+ %my-base-packagess))
+  (services
+    (append
+      (list (service login-service-type my-motd)
+            (service network-manager-service-type)
+            (service openssh-service-type)
+            (service unattended-upgrade-service-type)
+      %base-services))))
+
+;;; honeypot-config.scm ends here
