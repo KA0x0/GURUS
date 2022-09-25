19e45c8f81b533c0601924fddadbf2b5d12c25d5
KA0x0 <jasonmercier666@gmail.com>
2022-09-25 18:08:37 -0400

    Fixed formatting.

--- home/system/wsl-config.scm
+++ home/system/wsl-config.scm
@@ -2,45 +2,45 @@
 (use-service-modules mcron)
 (use-package-modules certs)
 
-  (operating-system
-    (host-name "wsl")
-    (timezone "America/New_York")
-    (bootloader
-     (bootloader-configuration
-      (bootloader dummy-bootloader)))
-    (kernel dummy-kernel)
-    (initrd dummy-initrd)
-    (initrd-modules '())
-    (firmware '())
-    (file-systems '())
-    (users (cons* (user-account
-                   (name "kracken")
-                   (group "users")
-                   (supplementary-groups '("wheel")) ; allow use of sudo
-                   (password "")
-                   (comment "Kracking hashes"))
-                  (user-account
-                   (inherit %root-account)
-                   (shell (wsl-boot-program "guest")))
-                  %base-user-accounts))
-    (packages
-      (append
-        (list
-        xf86-video-amdgpu
-        sshfs
-        xorg-server-xwayland
-    )))
-  %my-base-packagess))
-    (services
-     (list
-      (service login-service-type my-motd)
-      (service openssh-service-type)
-      (service unattended-upgrade-service-type)
-      (service guix-service-type)
-      (service special-files-service-type
-               `(("/bin/sh" ,(file-append bash "/bin/bash"))
-                 ("/bin/mount" ,(file-append util-linux "/bin/mount"))
-                 ("/usr/bin/env" ,(file-append coreutils "/bin/env"))
-                 %base-services))))))))))
+(operating-system
+   (host-name "wsl")
+   (timezone "America/New_York")
+   (bootloader
+    (bootloader-configuration
+     (bootloader dummy-bootloader)))
+   (kernel dummy-kernel)
+   (initrd dummy-initrd)
+   (initrd-modules '())
+   (firmware '())
+   (file-systems '())
+   (users (cons* (user-account
+                  (name "kracken")
+                  (group "users")
+                  (supplementary-groups '("wheel")) ; allow use of sudo
+                  (password "")
+                  (comment "Kracking hashes"))
+                 (user-account
+                  (inherit %root-account)
+                  (shell (wsl-boot-program "guest")))
+                 %base-user-accounts))
+   (packages
+     (append
+       (list
+       xf86-video-amdgpu
+       sshfs
+       xorg-server-xwayland
+   )))
+ %my-base-packagess))
+   (services
+    (list
+     (service login-service-type my-motd)
+     (service openssh-service-type)
+     (service unattended-upgrade-service-type)
+     (service guix-service-type)
+     (service special-files-service-type
+              `(("/bin/sh" ,(file-append bash "/bin/bash"))
+                ("/bin/mount" ,(file-append util-linux "/bin/mount"))
+                ("/usr/bin/env" ,(file-append coreutils "/bin/env"))
+                %base-services))))))))))
 
 ;;; wsl-config.scm ends here
