89f98fb40d30fb2465a79b8b6b72a04571ead0f7
KA0x0 <jasonmercier666@gmail.com>
2022-11-18 04:32:37 +0000

    Rename to match IP.

--- home/system/node1-config.scm
+++ home/system/node1-config.scm
@@ -6,7 +6,7 @@
   (locale "en_US.utf8")
   (timezone "America/New_York")
   (keyboard-layout (keyboard-layout "us" "ru"))
-  (host-name "node1")
+  (host-name "node50")
   (users (cons* (user-account
                   (name "vm")
                   (comment "Virtual Manager")
