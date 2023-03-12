58f0d2aedb8ff5790af45da286673b11bd75418b
KA0x0 <jasonmercier666@gmail.com>
2023-03-12 01:48:32 -0500

    Added --all to cd alias.

--- home/home.scm
+++ home/home.scm
@@ -20,7 +20,7 @@
     (list (service home-bash-service-type
             (home-bash-configuration
               (aliases ;; use "\" to escape aliases.
-               '(("cd" . "cd $@ && ls --color=auto --human-readable --indicator-style))
+               '(("cd" . "cd $@ && ls --all --color=auto --human-readable --indicator-style))
                  '(("cp" . "rsync --archive --human-readable --info=progress2 --progress --verbose --info=progress2"))
                  '(("curl" . "curl --verbose"))
                  '(("dd" . "dd status=progress"))
