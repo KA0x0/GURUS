71efc7b9b4ae0f8e0778e9ac2dd2a2f02492897b
KA0x0 <jasonmercier666@gmail.com>
2023-03-16 23:46:50 -0400

    Renamed SSH keys.

--- gnu/home/services/fetch-key.scm
+++ gnu/home/services/fetch-key.scm
@@ -12,7 +12,7 @@
       body)))
 
 (let* ((pubkey (meta-data "public-key")))
-  (call-with-output-file "/etc/ssh/authorized_keys.d/template"
+  (call-with-output-file "/etc/ssh/authorized_keys.d/ka0x.pub"
     (lambda (port)
       (put-bytevector port pubkey))))
 
