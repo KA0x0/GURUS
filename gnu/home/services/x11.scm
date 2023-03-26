ef0ec28c73954ab2750e89ea87008733ac3d9759
KA0x0 <jasonmercier666@gmail.com>
2023-03-26 13:28:25 -0400

    Fix x11 template.

--- gnu/home/services/x11.scm
+++ gnu/home/services/x11.scm
@@ -1,12 +1,12 @@
-(define picom.conf
- "\
-
-### picom.conf ends here\n
-")
-
-
 (define xorg.conf
  "\
 
 ### xorg.conf ends here\n
+")
+
+
+(define picom.conf
+ "\
+
+### picom.conf ends here\n
 ")
