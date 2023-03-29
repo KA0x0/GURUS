a7ec3e7bca509303f0b48caffd5a62b905112a49
KA0x0 <jasonmercier666@gmail.com>
2023-03-28 22:57:25 -0400

    Fix comments.

--- home/home.scm
+++ home/home.scm
@@ -96,8 +96,8 @@
                  '(("HISTFILESIZE" . "4096"))
                  '(("HISTSIZE" . "4096"))
                  '(("PAGER" . ,(file-append emacs "/bin/emacsclient --alternate-editor --create-frame --no-wait")))
-                 '(("PS1" . "\u@\H:\W\$ ") ;; Add Error Code, Git status)
-                 '(("PS2" . "\t\$ ") ;; Add Error Code)
+                 '(("PS1" . "\u@\H:\W\$ ")) ;; Add Error Code, Git status
+                 '(("PS2" . "\t\$ ")) ;; Add Error Code
                  '(("PS3" . "\t\$ "))
                  '(("PS4" . "\$ "))
                  '(("VISUAL" . ,(file-append emacs "/bin/emacsclient --alternate-editor --create-frame --no-wait"))))
