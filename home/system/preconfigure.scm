2650efef27e955b5be286929c971841cef48772b
KA0x0 <jasonmercier666@gmail.com>
2023-04-03 01:47:51 -0400

    Added preconfigure.scm.

---
+++ home/system/preconfigure.scm
@@ -0,0 +1,53 @@
+;;; Code:
+
+(use-modules (gnu) (guix) (guix packages) (srfi srfi-1))
+(use-service-modules mcron networking shepherd ssh)
+(use-package-modules bootloaders certs package-management)
+
+(operating-system
+  (locale "en_US.utf8")
+  (timezone "Etc/Greenwich")
+  (keyboard-layout (keyboard-layout "us" "ru"))
+  (host-name "configure")
+  (users (cons* (user-account
+                  (name "pre")
+                  (comment "Configure me")
+                  (group "users")
+                  (shell (file-append xonsh "/bin/xonsh"))
+                  (home-directory "/home/pre")
+                  (supplementary-groups
+                    '("netdev" "wheel")))
+                %base-user-accounts))
+  (bootloader
+    (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (target "/boot/efi")
+      (keyboard-layout keyboard-layout)))
+  (mapped-devices
+    (list (mapped-device
+            (source
+              (uuid ""))
+            (target "cryptroot")
+            (type luks-device-mapping))))
+  (file-systems
+    (cons* (file-system
+             (mount-point "/")
+             (device "/dev/mapper/cryptroot")
+             (type "bcachefs")
+             (dependencies mapped-devices))
+           (file-system
+             (mount-point "/boot/efi")
+             (device (uuid "" 'fat32))
+             (type "vfat"))
+           %base-file-systems))
+  (packages
+    (append
+     %my-base-packages))
+  (services
+    (append
+      (list 
+        (service network-manager-service-type)
+      %base-services
+      %my-base-services))))
+
+;;; preconfigure.scm ends here
