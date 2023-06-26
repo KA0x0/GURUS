46093ea9f826b7ac6843b60311a2bd64426b77c5
KA0x0 <jasonmercier666@gmail.com>
2023-06-26 00:28:55 -0400

    Name Change.

--- home/channels.scm
+++ home/channels.scm
@@ -2,7 +2,7 @@
  (list
   (channel
    (name 'channel-4-guix)
-   (url "https://git.sr.ht//channel-4-guix")
+   (url "https://git.sr.ht//C4G")
    (introduction
     (make-channel-introduction
      ""
