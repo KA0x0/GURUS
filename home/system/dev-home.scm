58ef2b9f86d2c9ca14af8f31c1c46028ff461d29
KA0x0 <jasonmercier666@gmail.com>
2023-08-22 23:32:11 -0400

    Fix ffcuesplit.

--- home/system/dev-home.scm
+++ home/system/dev-home.scm
@@ -21,8 +21,8 @@
               (aliases ;; use "\" to escape aliases.
                ("docker" . "podman")
                ("egrep" . "rg --engine auto --follow --smart-case")
+               ("ffcuesplit $1 $2" . "ffmpeg -i "$1" -f cue -i "$2" -map_metadata -1 -c copy -map 0 -map_metadata 1 "%02d - $1"")
                ("fgrep" . "rg --engine auto --fixed-strings --follow --smart-case")
-               ("ffmsplit" . "ffmpeg -i "$1" -f cue -i "$2" -map_metadata -1 -c copy -map 0 -map_metadata 1 "${1%.*}_%02d.${$1##*.}")
                ("rg" . "rg --engine auto --follow --smart-case")
                ("strings" . "floss -v"))
 
