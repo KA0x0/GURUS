8d21b38d8c08582abc8297f921315f1e038be600
KA0x0 <jasonmercier666@gmail.com>
2023-11-08 19:31:31 -0500

    Merge custom emacs packages.

--- gnu/packages/emacs.scm
+++ gnu/packages/emacs.scm
@@ -17,17 +17,14 @@
 
 ;;; Code:
 
-(define-module (my packages emacs)
+(define-module (packages my-emacs)
   #:use-module (guix packages)
-  #:use-module (guix memoization)
   #:use-module (guix git-download)
   #:use-module (guix utils)
   #:use-module (guix build utils)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages base)
   #:use-module (gnu packages emacs)
-  #:use-module (gnu packages gcc)
-  #:use-module (gnu packages webkit)
   #:use-module (gnu packages xorg)
   #:use-module (ice-9 regex)
   #:use-module (srfi srfi-1)
@@ -67,5 +64,97 @@
         `(modify-phases ,phases
            (delete 'restore-emacs-pdmp)
            (delete 'strip-double-wrap)))))))
+
+(define-public emacs-dirvish
+  (package
+    (name "emacs-dirvish")
+    (version "20230123.1201")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://github.com/alexluigit/dirvish.git")
+               (commit
+                 "4b63cd2e5ba994f8e674388db7035de1a8f0343f")))
+        (sha256
+          (base32
+            "1qp71p28h2k8xxpcsfxgdpgg587gaxwi6ialjwmhbd8qf07q4x3j"))))
+    (build-system melpa-build-system)
+    (arguments
+      '(#:files (:defaults "extensions/*.el")))
+    (home-page
+      "https://github.com/alexluigit/dirvish")
+    (synopsis
+      "A modern file manager based on dired mode")
+    (description
+      "Dirvish is an improved version of the Emacs inbuilt package Dired. It not only gives Dired an appealing and highly customizable user interface, but also comes together with almost all possible parts required for full usability as a modern file manager.")
+    (license license:gpl3)))
+
+(define-public emacs-jabber
+  (package
+    (name "emacs-jabber")
+    (version "20220713.1538")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://codeberg.org/emacs-jabber/emacs-jabber.git")
+               (commit
+                 "af0315e174fa6446d5c4dd3e6465d48912950e58")))
+        (sha256
+          (base32
+            "08q0hbm4pvp8sf261w1ihqa93sg8blfybfkhq7wrnvgs6kasgwvq"))))
+    (build-system melpa-build-system)
+    (propagated-inputs (list emacs-fsm emacs-srv))
+    (arguments
+      '(#:files
+        (:defaults
+          "*.org"
+          ("jabber-fallback-lib"
+           "jabber-fallback-lib/hexrgb.el"))))
+    (home-page
+      "https://codeberg.org/emacs-jabber/emacs-jabber")
+    (synopsis "A Jabber client for Emacs.")
+    (description
+      "This is jabber.el, an XMPP client for Emacs. XMPP (also known as 'Jabber') is an instant messaging system; see https://xmpp.org for more information.")
+    (license #f)))
+
+(define-public emacs-modbus
+  (package
+    (name "emacs-modbus")
+    (version "20200318.1618")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://github.com/spauldo/emacs-modbus.git")
+               (commit
+                 "d9005843b04ccfce19b53e0415834f72b7fbed4a")))))
+    (build-system melpa-build-system)
+    (home-page "https://github.com/spauldo/emacs-modbus")
+    (synopsis
+      "Modbus-mode for emacs")
+    (license license:gpl2)))
+
+(define-public emacs-verb
+  (package
+    (name "emacs-verb")
+    (version "20221113.2327")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://github.com/federicotdn/verb.git")
+               (commit
+                 "cb07a35bac5e2e3400d8f9e764177b9a9457deb1")))
+        (sha256
+          (base32
+            "1psah3k956hkl3qw3hjzfw89lcvcjp3zh6ap9ln0p9d8q696zmba"))))
+    (build-system melpa-build-system)
+    (home-page "https://github.com/federicotdn/verb")
+    (synopsis "Organize and send HTTP requests")
+    (description
+      "Verb is a package for Emacs which allows you to organize and send HTTP requests.")
+    (license license:gpl3)))
 
 ;;; emacs.scm ends here
