7d75e33a6bf6ca0e281f89c5dd84678cec073ad0
KA0x0 <jasonmercier666@gmail.com>
2023-11-14 23:33:54 -0500

    Fix emacs-scm.

--- gnu/home/services/emacs.scm
+++ gnu/home/services/emacs.scm
@@ -1,129 +1,127 @@
 (define guix-early-init.el
+  (require 'server)
+  (unless (server-running-p)
+    (server-start))
 
-(require 'server)
-(unless (server-running-p)
-  (server-start))
+  ;; Startup speed, annoyance suppression
+  (setq gc-cons-threshold 10000000)
+  (setq byte-compile-warnings '(not obsolete))
+  (setq warning-suppress-log-types '((comp) (bytecomp)))
+  (setq native-comp-async-report-warnings-errors 'silent)
 
-;; Startup speed, annoyance suppression
-(setq gc-cons-threshold 10000000)
-(setq byte-compile-warnings '(not obsolete))
-(setq warning-suppress-log-types '((comp) (bytecomp)))
-(setq native-comp-async-report-warnings-errors 'silent)
+  ;; Silence startup message
+  (setq inhibit-startup-echo-area-message (user-login-name))
 
-;; Silence startup message
-(setq inhibit-startup-echo-area-message (user-login-name))
+  ;; Default frame configuration: full screen, good-looking title bar on macOS
+  (setq frame-resize-pixelwise t)
+  (tool-bar-mode -1)                      ; All these tools are in the menu-bar anyway
+  (setq default-frame-alist '((fullscreen . maximized)
 
-;; Default frame configuration: full screen, good-looking title bar on macOS
-(setq frame-resize-pixelwise t)
-(tool-bar-mode -1)                      ; All these tools are in the menu-bar anyway
-(setq default-frame-alist '((fullscreen . maximized)
+  ;; Turn off scroll bars
+  (vertical-scroll-bars . nil)
+  (horizontal-scroll-bars . nil)
 
-;; Turn off scroll bars
-(vertical-scroll-bars . nil)
-(horizontal-scroll-bars . nil)
-
-;; Setting the face in here prevents flashes of
-;; color as the theme gets activated
-(background-color . "#000000")
-(ns-appearance . dark)
-(ns-transparent-titlebar . t)))
+  ;; Setting the face in here prevents flashes of
+  ;; color as the theme gets activated
+  (background-color . "#000000")
+  (ns-appearance . dark)
+  (ns-transparent-titlebar . t)))
 ")
 
 
-(define init.el
+(define guix-init.el
+  ;; UI:
+  (menu-bar-mode -1)
+  (toggle-scroll-bar -1)
+  (tool-bar-mode -1)
+  (blink-cursor-mode -1)
+  (column-number-mode)
 
-;; UI:
-(menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
-(blink-cursor-mode -1)
-(column-number-mode)
+  ;; Startup:
+  (setq-default set inhibit-startup-screen 1)
 
-;; Startup:
-(setq-default set inhibit-startup-screen 1)
+  ;; Interactively do things.
+  (ido-mode 1)
+  (ido-everywhere)
+  (setq-default ido-enable-flex-matching t)
+  (fido-mode)
 
-;; Interactively do things.
-(ido-mode 1)
-(ido-everywhere)
-(setq-default ido-enable-flex-matching t)
-(fido-mode)
+  ;; Vi emulation
+  (use-package meow)
+  (meow-setup)
+  (meow-global-mode 1)
 
-;; Vi emulation
-(use-package meow)
-(meow-setup)
-(meow-global-mode 1)
+  ;; Show stray whitespace.
+  (setq-default show-trailing-whitespace t)
+  (setq-default indicate-empty-lines t)
+  (setq-default indicate-buffer-boundaries 'left)
 
-;; Show stray whitespace.
-(setq-default show-trailing-whitespace t)
-(setq-default indicate-empty-lines t)
-(setq-default indicate-buffer-boundaries 'left)
+  ;; Add a newline automatically at the end of a file while saving.
+  (setq-default require-final-newline t)
 
-;; Add a newline automatically at the end of a file while saving.
-(setq-default require-final-newline t)
+  ;; Consider a period followed by a single space to be end of sentence.
+  (setq-default sentence-end-double-space nil)
 
-;; Consider a period followed by a single space to be end of sentence.
-(setq-default sentence-end-double-space nil)
+  ;; Use spaces, not tabs, for indentation.
+  (setq-default indent-tabs-mode nil)
 
-;; Use spaces, not tabs, for indentation.
-(setq-default indent-tabs-mode nil)
+  ;; Highlight matching pairs of parentheses.
+  (setq-default show-paren-delay 0)
+  (show-paren-mode)
 
-;; Highlight matching pairs of parentheses.
-(setq-default show-paren-delay 0)
-(show-paren-mode)
+  ;; Write auto-saves and backups to separate directory.
+  (make-directory "~/.tmp/emacs/auto-save/" t)
+  (setq-default auto-save-file-name-transforms '((".*" "~/.tmp/emacs/auto-save/" t)))
+  (setq-default backup-directory-alist '(("." . "~/.tmp/emacs/backup/")))
 
-;; Write auto-saves and backups to separate directory.
-(make-directory "~/.tmp/emacs/auto-save/" t)
-(setq-default auto-save-file-name-transforms '((".*" "~/.tmp/emacs/auto-save/" t)))
-(setq-default backup-directory-alist '(("." . "~/.tmp/emacs/backup/")))
+  ;; Do not move the current file while creating backup.
+  (setq-default backup-by-copying t)
 
-;; Do not move the current file while creating backup.
-(setq-default backup-by-copying t)
+  ;; Lockfiles.
+  (setq-default create-lockfiles nil) ;; Disable lockfiles
 
-;; Lockfiles.
-(setq-default create-lockfiles nil) ;; Disable lockfiles
+  ;; Electric Pair
+  (electric-pair-mode 1) ;; Enable Electric-Pair-mode
+  (setq-default electric-pair-delete-adjacent-pairs 1)
 
-;; Electric Pair
-(electric-pair-mode 1) ;; Enable Electric-Pair-mode
-(setq-default electric-pair-delete-adjacent-pairs 1)
+  ;; Eshell
+  (use-package eshell-syntax-highlighting
+    :after eshell-mode
+    :config
+    ;; Enable in all Eshell buffers.
+    (eshell-syntax-highlighting-global-mode +1))
 
-;; Eshell
-(use-package eshell-syntax-highlighting
-  :after eshell-mode
-  :config
-  ;; Enable in all Eshell buffers.
-  (eshell-syntax-highlighting-global-mode +1))
+  (add-to-list 'load-path (expand-file-name "emacs-client.scm"))
 
-(add-to-list 'load-path (expand-file-name "emacs-client.scm"))
-
-;;; init.el ends here\n
+  ;;; init.el ends here\n
 ")
 
 
 (define emacs-client.el
-;; Theme:
-(load-theme 'modus-vivendi)
-
-;; Fonts:
-(set-frame-font "fira code 12" nil t)
-;; Enable the www ligature in every possible major mode
-(ligature-set-ligatures 't '("www"))
-
-;; Enable ligatures in programming modes                                                           
-(ligature-set-ligatures 'prog-mode '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\" "{-" "::"
-                                     ":::" ":=" "!!" "!=" "!==" "-}" "----" "-->" "->" "->>"
-                                     "-<" "-<<" "-~" "#{" "#[" "##" "###" "####" "#(" "#?" "#_"
-                                     "#_(" ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*" "/**"
-                                     "/=" "/==" "/>" "//" "///" "&&" "||" "||=" "|=" "|>" "^=" "$>"
-                                     "++" "+++" "+>" "=:=" "==" "===" "==>" "=>" "=>>" "<="
-                                     "=<<" "=/=" ">-" ">=" ">=>" ">>" ">>-" ">>=" ">>>" "<*"
-                                     "<*>" "<|" "<|>" "<$" "<$>" "<!--" "<-" "<--" "<->" "<+"
-                                     "<+>" "<=" "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<"
-                                     "<~" "<~~" "</" "</>" "~@" "~-" "~>" "~~" "~~>" "%%"))
-
-(global-ligature-mode 't)
-
-;; Dired
-(use-package dirvish)
-
-;;; emacs-client.el ends here\n
+  ;; Theme:
+  (load-theme 'modus-vivendi)
+  
+  ;; Fonts:
+  (set-frame-font "fira code 12" nil t)
+  ;; Enable the www ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  
+  ;; Enable ligatures in programming modes                                                           
+  (ligature-set-ligatures 'prog-mode '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\" "{-" "::"
+                                       ":::" ":=" "!!" "!=" "!==" "-}" "----" "-->" "->" "->>"
+                                       "-<" "-<<" "-~" "#{" "#[" "##" "###" "####" "#(" "#?" "#_"
+                                       "#_(" ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*" "/**"
+                                       "/=" "/==" "/>" "//" "///" "&&" "||" "||=" "|=" "|>" "^=" "$>"
+                                       "++" "+++" "+>" "=:=" "==" "===" "==>" "=>" "=>>" "<="
+                                       "=<<" "=/=" ">-" ">=" ">=>" ">>" ">>-" ">>=" ">>>" "<*"
+                                       "<*>" "<|" "<|>" "<$" "<$>" "<!--" "<-" "<--" "<->" "<+"
+                                       "<+>" "<=" "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<"
+                                       "<~" "<~~" "</" "</>" "~@" "~-" "~>" "~~" "~~>" "%%"))
+  
+  (global-ligature-mode 't)
+  
+  ;; Dired
+  (use-package dirvish)
+  
+  ;;; emacs-client.el ends here\n
 ")
