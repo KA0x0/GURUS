82d2f4a1a996e27fa2c9d8141915240c772680a0
KA0x0 <jasonmercier666@gmail.com>
2023-12-26 16:22:10 -0500

    Added hosts services.

---
+++ gnu/home/services/hosts.scm
@@ -0,0 +1,26 @@
+(simple-service 'my-dns
+                    hosts-service-type
+                    (list (host "10.0.0.1"
+                                "network"
+                                "network.home"
+                                "network.home.arpa")
+                          (host "10.0.0.50"
+                                "node"
+                                "node.home"
+                                "node.home.arpa")))
+
+(simple-service 'my-redirect
+                    hosts-service-type
+                    (list (host "instagram.com"
+                                "")
+                          (host "twitter.com
+                                "")
+                          (host "youtube.com"
+                                "")))
+
+(simple-service 'my-adblock
+                    hosts-service-type
+                    (list (host ""
+                                "127.0.0.1")
+                          (host ""
+                                "127.0.0.1")))
