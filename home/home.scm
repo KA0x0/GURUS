81c2a64ed445a1fbab5cc312d03629a338caf36c
KA0x0 <jasonmercier666@gmail.com>
2024-04-14 18:13:56 -0400

    Updated PS.

--- home/home.scm
+++ home/home.scm
@@ -154,9 +154,9 @@
                 ("HISTFILESIZE" . "4096")
                 ("HISTSIZE" . "4096")
                 ("PAGER" . ,(file-append emacs "/bin/emacsclient --alternate-editor --create-frame --no-wait"))
-                ("PS1" . "\u@\H:\W\> ") ;; Add Error Code when =/ 0, Git status, Add mode 5 (Blinking)
-                ("PS2" . "\t\> ") ;; Add Error Code when =/ 0, Add mode 5 (Blinking)
-                ("PS3" . "\t\> ")
+                ("PS1" . "\[\e[91m\]\u\[\e[0m\]@\[\e[92m\]\H\[\e[0m\]:\[\e[94m\]\w\[\e[0;5m\]>\[\e[0m\] ") ;; Add Error Code when =/ 0, Git status
+                ("PS2" . "\[\e[3m\]\t\[\e[0;5m\]>\[\e[0m\] ")
+                ("PS3" . "\t? ")
                 ("PS4" . "\> ")
                 ("VISUAL" . ,(file-append emacs "/bin/emacsclient --alternate-editor --create-frame --no-wait")))
             (service home-shepherd-service-type
