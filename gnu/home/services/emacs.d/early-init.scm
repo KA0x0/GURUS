004d5526b0a4808fcede7961b57860332859b869
KA0x0 <jasonmercier666@gmail.com>
2024-07-19 00:18:04 -0400

    Remove obsolete gc-cons-threshold.

--- gnu/home/services/emacs.d/early-init.scm
+++ gnu/home/services/emacs.d/early-init.scm
@@ -4,7 +4,6 @@
     (server-start))
 
   ;; Startup speed, annoyance suppression
-  (setq gc-cons-threshold 10000000)
   (setq byte-compile-warnings '(not obsolete))
   (setq warning-suppress-log-types '((comp) (bytecomp)))
   (setq native-comp-async-report-warnings-errors 'silent)
