4a76ed6891066c5f631a37b343dbae2a7cba33c6
KA0x0 <jasonmercier666@gmail.com>
2024-08-01 22:13:43 -0400

    Switched to lambda sign.

--- home/home.scm
+++ home/home.scm
@@ -153,7 +153,7 @@
                 ("HISTFILESIZE" . "4096")
                 ("HISTSIZE" . "4096")
                 ("PAGER" . ,(file-append emacs "/bin/emacsclient --alternate-editor --create-frame --no-wait"))
-                ("PS1" . "\[\e[91m\]\u\[\e[0m\]@\[\e[92m\]\H\[\e[0m\]:\[\e[94m\]\w\[\e[0;5m\]>\[\e[0m\] ") ;; Add Error Code when =/ 0, Git status 𝛌
+                ("PS1" . "\[\e[91m\]\u\[\e[0m\]@\[\e[92m\]\H\[\e[0m\]:\[\e[94m\]\w\[\e[0;5m\]𝛌\[\e[0m\] ") ;; Add Error Code when =/ 0, Git status
                 ("PS2" . "\[\e[3m\]\t\[\e[0;5m\]>\[\e[0m\] ")
                 ("PS3" . "\t? ")
                 ("PS4" . "\> ")
