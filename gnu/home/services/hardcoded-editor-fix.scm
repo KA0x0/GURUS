48fdad60556fccada78a9b11955287fda5385f01
KA0x0 <jasonmercier666@gmail.com>
2024-08-25 02:43:35 -0400

    Fix comments.

--- gnu/home/services/hardcoded-editor-fix.scm
+++ gnu/home/services/hardcoded-editor-fix.scm
@@ -15,9 +15,9 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-;;; Code:
-
 ;; Fix Guix's hardcoded vi editor for tools like visudo.
+
+;;; Code:
 
 (use-modules 
 )
