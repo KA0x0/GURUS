7dad8e821dcd05f3a5f1278c18bca1465597946f
KA0x0 <jasonmercier666@gmail.com>
2024-10-25 23:57:36 +0000

    Added autoshell fuction

---
+++ gnu/home/services/dotfiles/bash.d/autoshell.sh
@@ -0,0 +1,7 @@
+#Run the given command via 'guix shell'
+function ,
+{
+    pkg_ver="$(set -o pipefail; guix locate "$1"|grep /bin/|head -1|cut -f1)"
+    pkg="$(echo $pkg_ver|cut-d@ -f1)"
+    test -n "$pkg" && guix shell "$pkg"--"$@"
+}
