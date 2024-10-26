6a53f35d3d3a4581c797ff0b5e7d9e5c14ecf914
KA0x0 <jasonmercier666@gmail.com>
2024-10-26 18:10:15 +0000

    Fix autoshell.sh

--- gnu/home/services/dotfiles/bash.d/autoshell.sh
+++ gnu/home/services/dotfiles/bash.d/autoshell.sh
@@ -2,6 +2,6 @@
 function ,
 {
     pkg_ver="$(set -o pipefail; guix locate "$1"|grep /bin/|head -1|cut -f1)"
-    pkg="$(echo $pkg_ver|cut-d@ -f1)"
+    pkg="$(echo $pkg_ver|cut -d@ -f1)"
     test -n "$pkg" && guix shell "$pkg"--"$@"
 }
