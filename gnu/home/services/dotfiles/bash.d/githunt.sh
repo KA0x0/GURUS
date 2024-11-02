2fae717123dd1155fa92ccadee8bbdae08b05073
KA0x0 <jasonmercier666@gmail.com>
2024-11-02 23:58:22 +0000

    Fix githunt.sh

--- gnu/home/services/dotfiles/bash.d/githunt.sh
+++ gnu/home/services/dotfiles/bash.d/githunt.sh
@@ -1,7 +1,5 @@
-githunt () 
-    {for commit in $(git log --all --pretty=format:%H -- $1);
-        do echo -n "$commit ";
-        git show $commit:$1 | md5sum;
-        done
-    | grep $2
-    }
+githunt () {
+    for commit in $(git log --all --pretty=format:%H -- $1); do
+        echo -n "$commit "$
+        git show $commit:$1| md5sum;| grep $2|awk '{print $1}'
+}
