2a99de536681fcf38c365ef1276f5190bfacbc6b
KA0x0 <jasonmercier666@gmail.com>
2025-01-04 04:34:28 +0000

    Added placeholder for confing not yet configurable with guix home. To confirm, and to keep to date.

--- home/home.scm
+++ home/home.scm
@@ -51,7 +51,8 @@
                ("PYTHONSTARTUP" . "~/config/python/ipython")
                ("PS1" . "\[\e[91m\]\u\[\e[0m\]@\[\e[92m\]\H\[\e[0m\]:\[\e[94m\]\w\[\e[0;5m\]$\[\e[0m\] ") ;; Add Error Code when =/ 0, Git status
                ("PS2" . "\[\e[3m\]\t\[\e[0;5m\]>\[\e[0m\] ")
-               ("VISUAL" . ,(file-append emacs "/bin/emacsclient --alternate-editor --create-frame --no-wait"))))
+               ("VISUAL" . ,(file-append emacs "/bin/emacsclient --alternate-editor --create-frame --no-wait"))
+               ("XDG_STATE_HOME" . " ")))
             (service home-dotfiles-service-type
               (home-dotfiles-configuration
                 (directories '("../gnu/home/services/config"))))
