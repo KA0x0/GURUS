edf7e31dbbe96ce95ae355cada4821f508e68f24
KA0x0 <jasonmercier666@gmail.com>
2025-01-04 05:14:34 +0000

    Delete Githunt, use magit.

--- gnu/home/services/config/bash.d/githunt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-trap 'echo "Warning: A command has failed. Exiting the script. Line was ($0:$LINENO): $(sed -n "${LINENO}p" "$0")"; exit 3' ERR
-set -Eeuo pipefail
-
-githunt () {
-    for commit in $(git log --all --pretty=format:%H -- $1); do
-        echo -n "$commit "$
-        git show $commit:$1|md5sum;|grep $2|awk '{print $1}'
-}
