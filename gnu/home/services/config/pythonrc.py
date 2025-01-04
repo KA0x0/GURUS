93045309dda20b11c136475c9a677825226f26d2
KA0x0 <jasonmercier666@gmail.com>
2025-01-04 02:34:35 +0000

    Tweaked Pythonrc.

--- gnu/home/services/config/pythonrc.py
+++ gnu/home/services/config/pythonrc.py
@@ -1,7 +1,6 @@
 import IPython
 import os
-import readline
 
 # https://ipython.readthedocs.io/en/stable/interactive/reference.html#ipython-as-your-default-python-environment
-IPython.start_ipython()
+IPython.start_ipython(argv=[])
 raise SystemExit
