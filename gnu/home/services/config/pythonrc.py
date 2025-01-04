8076a45c44b2e8ca46cec28d4fd418f762464e27
KA0x0 <jasonmercier666@gmail.com>
2025-01-04 02:08:07 +0000

    Added comments.

--- gnu/home/services/config/pythonrc.py
+++ gnu/home/services/config/pythonrc.py
@@ -2,5 +2,6 @@ import IPython
 import os
 import readline
 
+# https://ipython.readthedocs.io/en/stable/interactive/reference.html#ipython-as-your-default-python-environment
 IPython.start_ipython()
 raise SystemExit
