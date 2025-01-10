b0adceae89aae221a232f200e889f866b5c57768
KA0x0 <jasonmercier666@gmail.com>
2025-01-10 10:45:43 +0000

    Updated env-variable.

--- home/home.scm
+++ home/home.scm
@@ -44,15 +44,15 @@
                ("vdir"       . "emacsclient --alternate-editor --create-frame --eval '(dired)' --no-wait"))
               (environment-variables
                ("EDITOR"        . ,(file-append emacs "/bin/emacsclient --alternate-editor --create-frame --no-wait"))
-               ("HISTCONTROL"   . "ignoreboth")
-               ("HISTFILESIZE"  . "4096")
-               ("HISTSIZE"      . "4096")
+               ("HISTCONTROL"   . ,(literal-string "ignoreboth"))
+               ("HISTFILESIZE"  . ,(literal-string "4096"))
+               ("HISTSIZE"      . ,(literal-string "4096"))
                ("PAGER"         . ,(file-append emacs "/bin/emacsclient --alternate-editor --create-frame --no-wait"))
-               ("PYTHONSTARTUP" . "~/config/python/pythonrc.py")
-               ("IPYTHONDIR"    . "~/config/python/ipython")
-               ("PS1"           . "\[\e[91m\]\u\[\e[0m\]@\[\e[92m\]\H\[\e[0m\]:\[\e[94m\]\w\[\e[0;5m\]$\[\e[0m\] ") ;; Add Error Code when =/ 0, Git status
-               ("PS2"           . "\[\e[3m\]\t\[\e[0;5m\]>\[\e[0m\] ")
-               ("TMOUT"         . "898")
+               ("PYTHONSTARTUP" . ,(literal-string "/~/config/python/pythonrc.py"))
+               ("IPYTHONDIR"    . ,(literal-string "/~/config/python/ipython"))
+               ("PS1"           . ,(literal-string "\[\e[91m\]\u\[\e[0m\]@\[\e[92m\]\H\[\e[0m\]:\[\e[94m\]\w\[\e[0;5m\]$\[\e[0m\] ")) ;; Add Error Code when =/ 0, Git status
+               ("PS2"           . ,(literal-string "\[\e[3m\]\t\[\e[0;5m\]>\[\e[0m\] "))
+               ("TMOUT"         . ,(literal-string "898"))
                ("VISUAL"        . ,(file-append emacs "/bin/emacsclient --alternate-editor --create-frame --no-wait")))))
             (service home-dotfiles-service-type
               (home-dotfiles-configuration
