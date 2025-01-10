323277a50821bcf2135daf9ba76eab1e33789f66
KA0x0 <jasonmercier666@gmail.com>
2025-01-10 12:57:54 +0000

    Updated VISUAL

--- home/home.scm
+++ home/home.scm
@@ -49,13 +49,13 @@
                ("HISTCONTROL"      . "ignoreboth")
                ("HISTFILESIZE"     . "4096")
                ("HISTSIZE"         . "4096")
-               ("PAGER"            . "/~/.guix-profile/bin/emacsclient --create-frame --no-wait")
+               ("PAGER"            . "/~/.guix-profile/bin/emacsclient --create-frame --no-wait --eval '(man)'")
                ("PYTHONSTARTUP"    . "/~/config/python/pythonrc.py")
                ("IPYTHONDIR"       . "/~/config/python/ipython")
                ("PS1"              . ,(literal-string "\[\e[91m\]\u\[\e[0m\]@\[\e[92m\]\H\[\e[0m\]:\[\e[94m\]\w\[\e[0;5m\]$\[\e[0m\] ")) ;; Add Error Code when =/ 0, Git status
                ("PS2"              . ,(literal-string "\[\e[3m\]\t\[\e[0;5m\]>\[\e[0m\] "))
                ("TMOUT"            . "898")
-               ("VISUAL"           . "/~/.guix-profile/bin/emacsclient --create-frame --no-wait"))))
+               ("VISUAL"           . ../gnu/home/services/config/bash.d/emacs-pipe.sh"))))
             (service home-dotfiles-service-type
               (home-dotfiles-configuration
                 (directories '("../gnu/home/services/config"))))
