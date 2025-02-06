6f021bd4d1953be7263a4d15a0e253ecb8880b54
KA0x0 <jasonmercier666@gmail.com>
2025-02-06 04:43:25 +0000

    Added org.el

---
+++ gnu/home/services/config/emacs/lisp/org.el
@@ -0,0 +1,17 @@
+;;; org.el --- Org configurations -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package denote
+  :config
+  ;; Accept any symbol in a .dir-locals.el file; makes it easier to use silos.
+  ;; See "silos" in the manual: https://protesilaos.com/emacs/denote
+  (put 'denote-file-type 'safe-local-variable-p 'symbolp)
+)
+
+
+(provide 'my-org)
+
+;;; org.el ends here
