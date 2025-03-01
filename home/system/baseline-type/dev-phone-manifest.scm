dff64a3d6815d6ab9c53687c5cf00f66e3d49fb8
KA0x0 <jasonmercier666@gmail.com>
2025-03-01 17:24:58 -0500

    Add emacs-ultra-scroll to dev-phone-manifest.scm.

--- home/system/baseline-type/dev-phone-manifest.scm
+++ home/system/baseline-type/dev-phone-manifest.scm
@@ -33,6 +33,7 @@
         "emacs-org-caldav"
         "emacs-org-timeblock"
         "emacs-org-ql"
+        "emacs-ultra-scroll"
         "emacs-verb"
         "emacs-x509-mode"
         "emacs-yasnippet"
