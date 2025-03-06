5a84e2a7e57c3afb584ee2c42f60dfd0b2b60c72
KA0x0 <jasonmercier666@gmail.com>
2025-03-05 22:20:42 -0500

    Added guile.scm to emacs conf.

---
+++ gnu/home/services/config/emacs/site-lisp/guile.scm
@@ -0,0 +1,15 @@
+;;; guile.el --- Provide GNU Guile related features -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package eglot)
+
+(use-package geiser)
+(use-package geiser-guile)
+
+
+(provide 'my-guile)
+
+;;; guile.el ends here
