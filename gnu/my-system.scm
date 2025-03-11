0e0611b4766514350e6f85221d623eba99633925
KA0x0 <jasonmercier666@gmail.com>
2025-03-10 21:51:08 -0400

    Added comments.

--- gnu/my-system.scm
+++ gnu/my-system.scm
@@ -84,7 +84,7 @@
     "emacs-lispy"
     "emacs-logview"
     "emacs-lsp-booster" "emacs-eglot-booster"
-    "emacs-magit"
+    "emacs-magit" ;; pulls emacs-transient
     "emacs-marginalia"
     "emacs-orderless"
     "emacs-prism"
