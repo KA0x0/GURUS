4c68b81d6875b1bac80bac220e122d37ae7f33db
KA0x0 <jasonmercier666@gmail.com>
2025-05-15 13:08:34 -0400

    Emacs stuff.

--- gnu/home/services/config/emacs/site-lisp/templat.el
+++ gnu/home/services/config/emacs/site-lisp/templat.el
@@ -33,9 +33,8 @@
 
   ;; Optionally make the Tempel templates available to Abbrev,
   ;; either locally or globally. `expand-abbrev' is bound to C-x '.
-  ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
-  ;; (global-tempel-abbrev-mode)
-)
+  (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+  (global-tempel-abbrev-mode))
 
 (provide 'my-template)
 
