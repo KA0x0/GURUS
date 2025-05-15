4114ef1f80da79d6a3808a31bca5c24667d58253
KA0x0 <jasonmercier666@gmail.com>
2025-05-15 10:33:20 -0400

    Added placeholder.

---
+++ gnu/home/services/config/emacs/site-lisp/dev.el
@@ -0,0 +1,12 @@
+;;; dev.el --- Provide development related features -*- lexical-binding: t; -*-
+
+;;; Commentary: Small development packages that do not need a file
+
+;;; Code:
+
+(use-package eglot)
+
+
+(provide 'my-dev)
+
+;;; dev.el ends here
