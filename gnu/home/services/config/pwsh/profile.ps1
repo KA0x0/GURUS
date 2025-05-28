00aaba629a9f373786dcdfda24bee6001afb80d9
KA0x0 <jasonmercier666@gmail.com>
2025-05-28 16:22:09 -0400

    Added powershell prompt function.

--- gnu/home/services/config/pwsh/profile.ps1
+++ gnu/home/services/config/pwsh/profile.ps1
@@ -1,1 +1,5 @@
-# TODO : Auto expand aliases on return.
+function prompt {
+    $currentDirectory = $PWD.Path.Replace($HOME, "~")
+    $host.UI.RawUI.WindowTitle = $currentDirectory
+    $promptChar = if ($IsAdmin) {">_ " else "> "}
+}
