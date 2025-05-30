f5661099caa26343679fc45b2a779341653ad749
KA0x0 <jasonmercier666@gmail.com>
2025-05-30 18:34:33 -0400

    Cleanup profile.ps1.

--- gnu/home/services/config/pwsh/profile.ps1
+++ gnu/home/services/config/pwsh/profile.ps1
@@ -1,7 +1,5 @@
-function prompt {
-    $currentDirectory = $PWD.Path.Replace($HOME, "~")
-    $host.UI.RawUI.WindowTitle = $currentDirectory
-    $promptChar = if ($IsAdmin) {">_ " else "> "}
-}
-
 # TODO : Auto expand aliases on return.
+
+$currentDirectory = $PWD.Path.Replace($HOME, "~")
+$host.UI.RawUI.WindowTitle = $currentDirectory
+$promptChar = if ($IsAdmin) {">_ " else "> "}
