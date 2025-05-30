da5b795ca55aee6ada8d8c23da888725f5944255
KA0x0 <jasonmercier666@gmail.com>
2025-05-30 19:08:18 -0400

    Updated profile.ps1.

--- gnu/home/services/config/pwsh/profile.ps1
+++ gnu/home/services/config/pwsh/profile.ps1
@@ -1,7 +1,12 @@
 function prompt {
+    $red = [char27][31m
+    $green = [char27][32m
+    $blue = [char27][34m
+    $resetColor = [char27][0m
+
     $currentWorkingDirectory = $PWD.Path.Replace($HOME, "~")
     $host.UI.RawUI.WindowTitle = $currentWorkingDirectory
-    $checkPrivs = if ($IsAdmin) {">_ " else "> "}
+    $colorUser = if ($IsAdmin) {"$red" else "$green"}
     # TODO : Auto expand aliases on return.
-    return "$user@$currentWorkingDirectory$checkPrivs "
+    return "$colorUser@:$blue$currentWorkingDirectory$resetColor>_"
 }
