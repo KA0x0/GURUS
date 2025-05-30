28c2087c1316d4c58605ff27a6bb57ed816255bb
KA0x0 <jasonmercier666@gmail.com>
2025-05-30 18:46:46 -0400

    Cleanup powershell profile.

--- gnu/home/services/config/pwsh/profile.ps1
+++ gnu/home/services/config/pwsh/profile.ps1
@@ -1,5 +1,6 @@
-# TODO : Auto expand aliases on return.
-
-$currentDirectory = $PWD.Path.Replace($HOME, "~")
-$host.UI.RawUI.WindowTitle = $currentDirectory
-$promptChar = if ($IsAdmin) {">_ " else "> "}
+function prompt {
+    $currentDirectory = $PWD.Path.Replace($HOME, "~")
+    $host.UI.RawUI.WindowTitle = $currentDirectory
+    $promptChar = if ($IsAdmin) {">_ " else "> "}
+    # TODO : Auto expand aliases on return.
+}
