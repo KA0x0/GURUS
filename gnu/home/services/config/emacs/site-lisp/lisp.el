68ea9bd29641472bde320084e69979e0a7021202
KA0x0 <jasonmercier666@gmail.com>
2025-10-30 23:46:51 +0000

    Moved geiser config.

--- gnu/home/services/config/emacs/site-lisp/lisp.el
+++ gnu/home/services/config/emacs/site-lisp/lisp.el
@@ -5,15 +5,16 @@
 ;;; Code:
 
 (use-package eglot)
-
+(use-package geiser)
 (use-package prism)
+
+(setopt guix-geiser-connection-timeout (* 1000 60 30)) ;; 30 mins
 
 (use-package lispy
   :hook (emacs-lisp-mode . lispy-mode))
 
 (use-package flymake-guile
   :hook (scheme-mode-hook . flymake-guile))
-
 
 (provide 'my-lisp)
 
--- gnu/home/services/config/emacs/site-lisp/ui.el
+++ gnu/home/services/config/emacs/site-lisp/ui.el
@@ -62,8 +62,6 @@
 :init
   (exec-path-from-shell-initialize))
 
-(setopt guix-geiser-connection-timeout (* 1000 60 30)) ;; 30 mins
-
 (use-package diff-hl
   :config
   (global-diff-hl-mode))
