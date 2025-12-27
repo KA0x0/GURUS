a37a8b02188d61250d7b5c0f3606ef58dfadad66
KA0x0 <jasonmercier666@gmail.com>
2025-12-27 14:56:31 +0000

    Misc.

--- home/channels.scm
+++ home/channels.scm
@@ -1,12 +1,33 @@
-(append
- (list
-  (channel
-   (name 'GURU)
-   (url "https://github.com/KA0x0/GURU")
-   (introduction
-    (make-channel-introduction
-     ""
-     (openpgp-fingerprint
-      "")))))
- %default-channels)
- 
+;;; GNU Guix --- Functional package management for GNU
+;;;
+;;; This file is NOT part of GNU Guix.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(cons* (channel
+        (name 'GURUS)
+        (url "https://github.com/KA0x0/GURUS"))
+       (channel 
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (introduction
+         (make-channel-introduction
+          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+          (openpgp-fingerprint
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       %default-channels)
+
+;;; channels.scm ends here
