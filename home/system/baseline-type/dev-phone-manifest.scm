1f5a436b425cb4350426d9fb15ff04fc38f56e47
KA0x0 <jasonmercier666@gmail.com>
2026-01-11 12:06:44 +0000

    Added emacs-varuga to dev-phone-manifest.scm.

--- home/system/baseline-type/dev-phone-manifest.scm
+++ home/system/baseline-type/dev-phone-manifest.scm
@@ -48,6 +48,7 @@
         "emacs-osm"
         "emacs-polymode" "emacs-polymode-org"
         "emacs-tempel" "emacs-eglot-tempel"
+        "emacs-varuga"
         "emacs-verb"
         "emacs-x509-mode"
         "file"
